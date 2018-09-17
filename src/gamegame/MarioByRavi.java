94fd7f9e5ec69530384af2152350cc4d3bb3b6e5
Ravi gupta <Guptaravi0719@gmail.com>
2018-09-17 21:28:58 +0530

    commit at 9:28  9/17/2018

--- src/gamegame/MarioByRavi.java
+++ src/gamegame/MarioByRavi.java
@@ -9,13 +9,13 @@ import java.util.Random;
 
 
 import javax.imageio.ImageIO;
-
 import javax.swing.*;
 
 
+public class MarioByRavi {
 
-    public class MarioByRavi {
     public static void main(String[] args) {
+
         JFrame frame = new JFrame("Mario Project by Ravi");
         frame.setBackground(Color.CYAN);
         frame.setResizable(false);
@@ -24,18 +24,21 @@ import javax.swing.*;
         frame.setSize(600, 430);
         frame.setVisible(true);
     }}
-
-class mario {
+    class mario { public static boolean condition=false,bol=false;
+    Graphics g;
     Random random=new Random();
     int x, xvel, yvel, y,xfirstback,xsecondback,
             yenemy1=285,yenemy2=285,yenemy3=285,yenemy4=285,yenemy5=285,yenemy6=285;
-
+          int score=0;
     int xenemy1= 1000+random.nextInt(5000);
     int xenemy2=1000+random.nextInt(5000);
    int xenemy3= 1000+random.nextInt(5000);
     int xenemy4=1200;
     int xenemy5 =3500;
     int xenemy6= 7000;
+    int xenemy7=5000;
+    int xenemy8=6000;
+
     Image img2=null;
     Image imgenemy1=null;
     Image  imgenemy2=null;
@@ -43,7 +46,8 @@ class mario {
     Image imgenemy4=null;
     Image imgenemy5=null;
     Image imgenemy6=null;
-
+    Image imgenemy7=null;
+Image  imgenemy8=null;
     public mario() {
         try {
             img2 = ImageIO.read(board.class.getClassLoader().getResource("image/Zxfbi.png"));
@@ -53,7 +57,9 @@ class mario {
             imgenemy4 = ImageIO.read(board.class.getClassLoader().getResource("image/grub1.png"));
             imgenemy5 = ImageIO.read(board.class.getClassLoader().getResource("image/fly1 - Copy (3).png"));
             imgenemy6 = ImageIO.read(board.class.getClassLoader().getResource("image/grub1 - Copy - Copy (2).png "));
-
+            imgenemy6 = ImageIO.read(board.class.getClassLoader().getResource("image/grub1 - Copy - Copy (2).png "));
+            imgenemy7= ImageIO.read(board.class.getClassLoader().getResource("image/grub1 - Copy - Copy (2).png "));
+            imgenemy8 = ImageIO.read(board.class.getClassLoader().getResource("image/grub1 - Copy - Copy (2).png "));
         } catch (IOException ev) {
             ev.printStackTrace(); }
         x = 50;
@@ -64,6 +70,7 @@ class mario {
     }
 
     public void move() {
+
         y+=yvel;
         xfirstback-=xvel;
         xsecondback-=xvel;
@@ -72,75 +79,95 @@ class mario {
         xenemy5=xenemy5-1;
         xenemy6=xenemy6 -1;
       xenemy1=xenemy1-1;
-      xenemy2=xenemy2-1;
-      if(xfirstback==0) {
-          xsecondback = 600; //infinite back
-      }
-      if(xsecondback==0){
-          xfirstback=600;
-      }
-      if(x==xenemy1&&y==285){
-          System.out.println("GAME OVER");
-          java.lang.System.exit(0);
-      }
+     xenemy2=xenemy2-1;
+        if(xfirstback==0) {
+            xsecondback = 600; //infinite back
+        }
+        if(xsecondback==0){
+            xfirstback=600;
+        }
+        if(x==xenemy1&&y==285){
+condition=true;
+bol=true;
+        }
+
         if (x == xenemy2 && y == 285) {
-            System.out.println("GAME OVER");
-            java.lang.System.exit(0);
+            condition=true;
+            bol=true;
         }
         if(x==xenemy3&&y==285){
-            System.out.println("GAME OVER");
-            java.lang.System.exit(0);
+            condition=true;
+bol=true;
         }
-        if (x == xenemy4 && y == 285) {
-            System.out.println("GAME OVER");
-            java.lang.System.exit(0);
+        if (x ==xenemy4 && y == 285) {
+            condition=true;
+            bol=true;
         }
         if(x==xenemy5&&y==285){
-            System.out.println("GAME OVER");
-          java.lang.System.exit(0);
+            condition=true;
+            bol=true;
         }
         if (x == xenemy6 && y == 285) {
-            System.out.println("GAME OVER");java.lang.System.exit(0);
+            condition=true;
+            bol=true;
+
         }
+
+
     }
 
-    public void keyPressed(KeyEvent e) {
+
+
+
+    public void keyPressed(KeyEvent e) {       if(bol==true)
+    {
+        return;
+    }
         int key = e.getKeyCode();
 
-        if(key==KeyEvent.VK_SPACE)
-            y=220;
+         if(key==KeyEvent.VK_SPACE) {
+            y = 220;
+
+
+
+        }
 
 
 
         if (key == KeyEvent.VK_RIGHT){
 
-            xvel=2;
+             xvel=2;
  }}
-    public void keyReleased(KeyEvent e){
+    public void keyReleased(KeyEvent e){       if(bol==true)
+    {
+        return;
+    }
 
         int key=e.getKeyCode();
         if(key==KeyEvent.VK_LEFT)
             xvel=0;
-        if(key== KeyEvent.VK_SPACE)
-            y = 285;
+           if(key== KeyEvent.VK_SPACE)
+               y=285;
+
+
         if(key==KeyEvent.VK_RIGHT)
             xvel=0;
     }}
 
 
-    class board  extends JPanel implements ActionListener {
+    class board  extends JPanel implements ActionListener { int score;
     mario p  ;
     Image imgback1=null,imgback2=null;
     Timer time;
 
-
+Image over;
     public board() {
         addKeyListener(new for_key_methods_in_mario());
         setFocusable(true);
         p = new mario();
 
         try {
-
+over=ImageIO.read(MarioByRavi.class.getClassLoader().getResource("image/rsz_game-over-png-5.png"));
             imgback1 = ImageIO.read(MarioByRavi.class.getClassLoader().getResource("image/WHu9Z.png"));
             imgback2= ImageIO.read(MarioByRavi.class.getClassLoader().getResource("image/WHu9Z.png"));
         } catch (IOException ev) {
@@ -154,11 +181,16 @@ class mario {
     public void actionPerformed(ActionEvent e) {
         p.move();
         repaint();
+        if(p.bol==true)
+        {
+            return;
+        }
           }
 
 
-    public void paint(Graphics g) {
+    public void paint(Graphics g) {g.setColor(Color.RED);
 
+    g.setFont(new Font("Courier new",Font.BOLD,30));
         g.drawImage(imgback1, p.xfirstback, 0, null);//make same object of graphic class only
         g.drawImage(imgback2, p.xsecondback, 0, null);
         g.drawImage(p.img2, p.x, p.y, null);
@@ -168,22 +200,40 @@ class mario {
         g.drawImage(p.imgenemy4, p.xenemy4, p.yenemy4, null);
         g.drawImage(p.imgenemy5, p.xenemy5, p.yenemy5, null);
         g.drawImage(p.imgenemy6, p.xenemy6, p.yenemy6, null);
+        g.drawImage(p.imgenemy7, p.xenemy5, p.yenemy5, null);
+        g.drawString("score "+"   "+score,20,20);
+if(p.condition==true){  g.setColor(Color.green);
+
+    g.setFont(new Font("Courier new",Font.BOLD,50));
+    g.drawImage(imgback1, p.xfirstback, 0, null);
+    g.drawImage(over,0,0,null);
+g.drawString("max score is"+" "+score,50,50);
+
+
+
+
+}
+
 
     }
 
 
     class for_key_methods_in_mario extends KeyAdapter {
 
-          public void keyReleased(KeyEvent e){
+          public void keyReleased(KeyEvent e){  // if(p.x>p.xenemy1||p.x>p.xenemy2||p.x>p.xenemy3||p.x>p.xenemy4||p.x>p.xenemy5||p.x>p.xenemy6)
+         // {
+            //  score=score+100;
 
+         // }
             p.keyReleased(e);
            }
            public void keyPressed(KeyEvent e){
+               score+=1;
             p.keyPressed(e);
-
+            if(p.bol==true)
+            {
+               return;
              }
-          }
-
-}
+          }}}
 
 
